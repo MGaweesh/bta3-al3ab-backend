5f0235b3e57864e6c8e2413c4c5a530de7acea9a
MGaweesh <gawesh1112@gmail.com>
2025-11-24 20:46:25 +0200

    Add files via upload

--- db/mongodb.js
+++ db/mongodb.js
@@ -17,7 +17,15 @@ const connectDB = async () => {
       return db;
     }
 
-    client = new MongoClient(uri);
+    // Connection options for better performance
+    const options = {
+      maxPoolSize: 10, // Maintain up to 10 socket connections
+      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+    };
+    
+    client = new MongoClient(uri, options);
     await client.connect();
     // Use database name from URI or default to 'bta3al3ab'
     const dbName = uri.match(/mongodb\+srv:\/\/[^/]+\/([^?]+)/)?.[1] || 'bta3al3ab';
