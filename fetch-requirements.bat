eddb41db7895b2b1d3358685d243152bbf524004
MGaweesh <gawesh1112@gmail.com>
2025-12-17 23:36:57 +0200

    Delete fetch-requirements.bat

--- fetch-requirements.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo Fetching Requirements for All Games
-echo ========================================
-echo.
-cd /d "%~dp0"
-node scripts/fetch-all-requirements-now.js
-pause
-
